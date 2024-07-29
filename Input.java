39e7f8fa2559e5d92f8eaf65838e4a8b50cee905
123 <123>
2024-07-30 02:39:28 +0300

    Commit

--- Input.java
+++ Input.java
@@ -2,7 +2,7 @@ public class Input {
     private final CalcComplex cA = new CalcComplex( 4.0, -2.0 );
     private final CalcComplex cB = new CalcComplex( 2.5, 1.5 );
     private final CalcComplex[] nums 
-        = new CalcComplex[] {cA,cB};// на случай если чесел больше 2ч
+        = new CalcComplex[] {cA,cB};// на случай если чисел больше 2ч
     
 
     public CalcComplex getA() {
